11580fc6bede31941f4bb6e8491cad902e6a8b45
Marcosta23 <mpc2073@yahoo.com.br>
2022-02-14 23:47:52 -0300

    create food

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8*box
 }
 let direction = "right"; 
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBG(){
     context. fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -18,6 +22,11 @@ for (i=0;i< snake.length; i++){
     context. fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box,box);
 }
+}
+
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -36,6 +45,7 @@ function startGame(){
 
     criarBG();
     createSnake();
+    drawFood();
     let snakeX = snake[0].x;
     let snakeY =  snake[0].y;
      if (direction =="right") snakeX += box
